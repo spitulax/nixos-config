b3dbf035b72cf93a0982348d39b2fe0c9375e216
Bintang <bintangadiputrapratama@gmail.com>
2024-03-09 22:51:51 +0700

    Fix bluetooth

--- modules/nixos/common/bluetooth.nix
+++ modules/nixos/common/bluetooth.nix
@@ -4,9 +4,15 @@
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = false;
+    settings = {
+      Policy = {
+        AutoEnable = false;
+      };
+    };
   };
   environment.etc."bluetooth/audio.conf".text = ''
     [General]
     Enable=Source
   '';
+  services.blueman.enable = true;
 }
