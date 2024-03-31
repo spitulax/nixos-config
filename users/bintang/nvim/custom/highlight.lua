22de4cf87b577ea16f8c4c5d003e6753d341677d
Bintang <bintangadiputrapratama@gmail.com>
2024-03-31 22:56:57 +0700

    [nvim] Update highlights

--- users/bintang/nvim/custom/highlight.lua
+++ users/bintang/nvim/custom/highlight.lua
@@ -15,8 +15,8 @@ M.override = {
 ---@type Base46HLGroupsList
 M.add = {
   NvimTreeModifiedFile = { fg = "orange" },
-  CurSearch = { fg = "black", bg = "orange" },
-  IncSearch = { fg = "black", bg = "orange" },
+  CurSearch = { fg = "black", bg = "red" },
+  IncSearch = { fg = "black", bg = "red" },
   Search = { fg = "black", bg = "yellow" },
   CursorLine = { bg = "one_bg" },
   GitSignsCurrentLineBlame = { link = "Comment" },
