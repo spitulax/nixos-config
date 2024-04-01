65ee96657b05ad6b15ec760504ad79d346f115f3
Bintang <bintangadiputrapratama@gmail.com>
2024-04-01 15:34:17 +0700

    Add templates

---
+++ templates/c/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A C/C++ project";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forEachSystem = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system} system);
+    in
+    {
+      packages = forEachSystem (pkgs: system: {
+        default = with pkgs; stdenv.mkDerivation {
+          pname = "foobar";
+          version = "0.1.0";
+          src = ./.;
+
+          nativeBuildInputs = [
+
+          ];
+
+          buildInputs = [
+
+          ];
+
+          makeFlags = [ "PREFIX=$(out)" ];
+        };
+      });
+
+      devShells = forEachSystem (pkgs: system:
+        builtins.mapAttrs
+          (_: p:
+            p.overrideAttrs (finalAttrs: prevAttrs: with pkgs; {
+              nativeBuildInputs = [
+
+              ] ++ (prevAttrs.nativeBuildInputs or [ ]);
+            })
+          )
+          self.packages.${system}
+      );
+    };
+}
