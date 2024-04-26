35b8dfd6fc807fab3f9bec29f9a66c6b4a161ed6
Bintang <bintangadiputrapratama@gmail.com>
2024-04-26 23:00:27 +0700

    [waybar] Add some more infos to custom-menu tooltip

--- users/bintang/desktop/waybar/scripts/menu.sh
+++ users/bintang/desktop/waybar/scripts/menu.sh
@@ -12,9 +12,16 @@ UPTIME_FULL=$(printf "%sh %smin" "$UPTIME_H" "$UPTIME_MI")
 [[ $UPTIME_M > 0 ]] && UPTIME_FULL=$(printf "%smo %s" "$UPTIME_M" "$UPTIME_FULL")
 [[ $UPTIME_Y > 0 ]] && UPTIME_FULL=$(printf "%sy %s" "$UPTIME_Y" "$UPTIME_FULL")
 
-TOOLTIP=$(printf "Uptime %s\\\\nLinux  %s\\\\nNixOS  %s" \
+DISK_USED=$(df -h --total | tail -1 | awk '{print $3}')
+DISK_AVAIL=$(df -h --total | tail -1 | awk '{print $4}')
+DISK_AVAIL_P=$(df -h --total | tail -1 | awk '{print $5}')
+
+TOOLTIP=$(printf "Uptime: %s\\\\nLinux: %s\\\\nNixOS: %s\\\\nHyprland: %s\\\\nWaybar: %s\\\\nDisk: %s" \
   "$UPTIME_FULL" \
   "$(uname -r)" \
   "$(nixos-version)" \
+  "$(readlink -f $(which Hyprland) | sed 's/^.*hyprland-\(.*\)\/bin\/.*/\1/')" \
+  "$(readlink -f $(which waybar) | sed 's/^.*waybar-\(.*\)\/bin\/.*/\1/')" \
+  "$DISK_USED : $DISK_AVAIL ($DISK_AVAIL_P)" \
   )
 echo '{"text":"","tooltip":'"\"$TOOLTIP\"}"
