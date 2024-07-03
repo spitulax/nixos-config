98a46151cc768dcfe1ae338b10a19e2fc76e0540
Bintang <bintangadiputrapratama@gmail.com>
2024-07-04 00:49:21 +0700

    update
    • Updated input 'home-manager':
        'github:nix-community/home-manager/59ce796b2563e19821361abbe2067c3bb4143a7d' (2024-07-01)
      → 'github:nix-community/home-manager/36e2f9da91ce8b63a549a47688ae60d47c50de4b' (2024-07-03)
    • Updated input 'hyprland':
        'git+https://github.com/hyprwm/Hyprland?ref=refs/heads/main&rev=d7ea1b7785f31e9fd6477490309721a19a835592' (2024-06-30)
      → 'git+https://github.com/hyprwm/Hyprland?ref=refs/heads/main&rev=8bb75a223db3ea9471d05d74fbed3328334a9f78' (2024-07-02)
    • Updated input 'mypkgs':
        'github:spitulax/mypkgs/b9ec92ce8e4c718b0061e9eac1dac5f690e59be0' (2024-07-01)
      → 'github:spitulax/mypkgs/608238ee6c7df8d69acd0ba34b0c020242430845' (2024-07-03)
    • Updated input 'mypkgs/gripper':
        'github:spitulax/gripper/ed99d7249baed79aa8d15d0448930a0d1d42499f' (2024-06-02)
      → 'github:spitulax/gripper/98e3116473045e1d7a7909c3224f6ff61f639a6f' (2024-07-03)
    • Updated input 'mypkgs/hyprlock':
        'github:hyprwm/hyprlock/0f66aeac03ae4dda5223768c31ce720b9272e1db' (2024-06-28)
      → 'github:hyprwm/hyprlock/88b9ce48ed0c561c44c3a09cd6cef0e1bebaf59f' (2024-07-01)
    • Updated input 'mypkgs/nixpkgs':
        'github:nixos/nixpkgs/2741b4b489b55df32afac57bc4bfd220e8bf617e' (2024-06-29)
      → 'github:nixos/nixpkgs/00d80d13810dbfea8ab4ed1009b09100cca86ba8' (2024-07-01)
    • Updated input 'mypkgs/odin':
        'github:odin-lang/Odin/a2b23de0a72ad34f09d11c88fcee4062a54b554e' (2024-07-01)
      → 'github:odin-lang/Odin/075384b2bb9e55c1ed58463fefc6730947151e3a' (2024-07-02)
    • Updated input 'mypkgs/ols':
        'github:DanielGavin/ols/9fc36bfa5c76bec6f890d744398169a8069ada5a' (2024-06-28)
      → 'github:DanielGavin/ols/ee368d473146d33e89f9be9f754fd6d0ee08ecc6' (2024-07-01)
    • Updated input 'mypkgs/waybar':
        'github:Alexays/Waybar/99971556178ae504b7ba7ab559952f781925f7da' (2024-07-01)
      → 'github:Alexays/Waybar/6f994c849d6ee36ae3e24dd17c7cef1a7f288e94' (2024-07-03)
    • Updated input 'nixos-hardware':
        'github:NixOS/nixos-hardware/a59f00f5ac65b19382617ba00f360f8bc07ed3ac' (2024-06-29)
      → 'github:NixOS/nixos-hardware/6e253f12b1009053eff5344be5e835f604bb64cd' (2024-07-02)
    • Updated input 'nixpkgs-unstable':
        'github:nixos/nixpkgs/2741b4b489b55df32afac57bc4bfd220e8bf617e' (2024-06-29)
      → 'github:nixos/nixpkgs/00d80d13810dbfea8ab4ed1009b09100cca86ba8' (2024-07-01)
    • Updated input 'sops-nix':
        'github:Mic92/sops-nix/b5974d4331fb6c893e808977a2e1a6d34b3162d6' (2024-06-30)
      → 'github:Mic92/sops-nix/a11224af8d824935f363928074b4717ca2e280db' (2024-07-01)

--- modules/home/bintang/nvim/lua/plugins/auto-session.lua
+++
@@ -1,9 +0,0 @@
---- Saves session state so neovim will get back to the state of previous session after it has been closed.
-
----@type PluginConfig
-return {
-  spec = {
-    "rmagatti/auto-session",
-    lazy = false,
-  },
-}
