678c310590f5b569cd94491b6cb441dd1a167983
Bintang Adiputra Pratama <bintangadiputra@proton.me>
2024-08-25 13:39:55 +0700

    move common packages to global flake vars

--- config/nixos/common/packages.nix
+++ config/nixos/common/packages.nix
@@ -1,49 +1,10 @@
 { config
 , pkgs
+, outputs
 , ...
 }:
 {
-  environment.systemPackages = with pkgs; [
-    # shell utils
-    neovim
-    wget
-    curl
-    fd
-    fzf
-    which
-    file
-    tldr
-    git
-    mediainfo
-    lsof
-    exiftool
-    nix-tree
-
-    # monitoring
-    htop
-    iotop
-    iftop
-    powertop
-
-    # archives
-    zip
-    unzip
-    xz
-    p7zip
-
-    # system utils
-    inxi
-    cachix
-    psmisc
-    lm_sensors
-    ethtool
-    pciutils
-    usbutils
-    hdparm
-    dmidecode
-    parted
-    acpi
-  ] ++ config.configs.extraPackages;
+  environment.systemPackages = outputs.vars.commonPackage pkgs ++ config.configs.extraPackages;
   programs.nix-ld.enable = false;
   programs.fish.enable = true;
   programs.dconf.enable = true;
--- flake/vars.nix
+++ flake/vars.nix
@@ -22,4 +22,46 @@
   globalSecretsPath = ../secrets/global;
   hostsSecretsPath = ../secrets/hosts;
   usersSecretsPath = ../secrets/users;
+
+  commonPackage = pkgs: with pkgs; [
+    # shell utils
+    neovim
+    wget
+    curl
+    fd
+    fzf
+    which
+    file
+    tldr
+    git
+    mediainfo
+    lsof
+    exiftool
+    nix-tree
+
+    # monitoring
+    htop
+    iotop
+    iftop
+    powertop
+
+    # archives
+    zip
+    unzip
+    xz
+    p7zip
+
+    # system utils
+    inxi
+    cachix
+    psmisc
+    lm_sensors
+    ethtool
+    pciutils
+    usbutils
+    hdparm
+    dmidecode
+    parted
+    acpi
+  ];
 }
