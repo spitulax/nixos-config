2a230b1c00ad7f7070f0617010347e00a2698733
Bintang Adiputra Pratama <bintangadiputra@proton.me>
2025-01-03 01:34:38 +0700

    remove yazi cache

--- flake/vars.nix
+++ flake/vars.nix
@@ -7,7 +7,7 @@
     "https://nix-on-droid.cachix.org"
     "https://nix-community.cachix.org"
     "https://spitulax.cachix.org"
-    "https://yazi.cachix.org"
+    # "https://yazi.cachix.org"
   ];
   trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
@@ -16,7 +16,7 @@
     "nix-on-droid.cachix.org-1:56snoMJTXmDRC1Ei24CmKoUqvHJ9XCp+nidK7qkMQrU="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "spitulax.cachix.org-1:GQRdtUgc9vwHTkfukneFHFXLPOo0G/2lj2nRw66ENmU="
-    "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
+    # "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
   ];
 
   globalSecretsPath = ../secrets/global;
