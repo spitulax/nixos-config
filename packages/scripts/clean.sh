53c2bf48fc5256fd8a427b7d060fbd3596f9588a
Bintang Adiputra Pratama <bintangadiputra@proton.me>
2025-01-03 18:31:17 +0700

    packages/scripts: refactor clean.sh

--- packages/scripts/clean.sh
+++ packages/scripts/clean.sh
@@ -2,34 +2,41 @@
 
 set -e
 
+OLDER_THAN=${1:-3d}
+if [ "$OLDER_THAN" == "all" ]; then
+    FLAGS=""
+else
+    FLAGS="--older-than $OLDER_THAN"
+fi
+
 echo "Run this script after rebooting for the best result."
 sudo echo
 
 printf "\033[1;31mPress ENTER to delete \`result\` links...\n"
 printf "\033[0m"
 read
-for i in $(find $HOME -name 'result' -type l); do
+for i in $(fd -t l '^result$' $HOME && fd -t l '^result$' /tmp/nh-*); do
     echo "Deleting $i"
-    unlink $i
+    sudo unlink $i
 done
 echo
 
 printf "\033[1;31mPress ENTER to delete home-manager profiles...\n"
 printf "\033[0m"
 read
-nix profile wipe-history --debug --older-than 3d --profile $XDG_STATE_HOME/nix/profiles/home-manager
+nix profile wipe-history --debug $FLAGS --profile $XDG_STATE_HOME/nix/profiles/home-manager
 echo
 
 printf "\033[1;31mPress ENTER to delete user profiles...\n"
 printf "\033[0m"
 read
-nix profile wipe-history --debug --older-than 3d --profile $XDG_STATE_HOME/nix/profiles/profile
+nix profile wipe-history --debug $FLAGS --profile $XDG_STATE_HOME/nix/profiles/profile
 echo
 
 printf "\033[1;31mPress ENTER to delete system profiles...\n"
 printf "\033[0m"
 read
-sudo nix profile wipe-history --debug --older-than 3d --profile /nix/var/nix/profiles/system
+sudo nix profile wipe-history --debug $FLAGS --profile /nix/var/nix/profiles/system
 echo
 
 printf "\033[1;31mPress ENTER to run nix store gc...\n"
