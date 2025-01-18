ec5e32998ae2d7061d510443be882e0b877d858e
Bintang Adiputra Pratama <bintangadiputra@proton.me>
2025-01-19 05:20:45 +0700

    home/bintang/apps: fix distrobox env

--- config/home/bintang/apps/distrobox.nix
+++ config/home/bintang/apps/distrobox.nix
@@ -5,6 +5,28 @@
 }:
 let
   cfg = config.configs.apps.distrobox;
+
+  # Fix distrobox environment with `exec distrobox-env {container-name}`
+  distrobox-env = pkgs.writeShellScriptBin "distrobox-env" ''
+    PROFILE=''${1:-default}
+
+    if [ -d "$HOME/.local/bin/distrobox/$PROFILE" ]; then
+      PATH="$HOME/.local/bin/distrobox/$PROFILE:$PATH"
+    fi
+    PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH"
+    export PATH
+
+    if [ -d "$HOME/.local/lib/distrobox/$PROFILE" ]; then
+      LD_LIBRARY_PATH="$HOME/.local/lib/distrobox/$PROFILE:$LD_LIBRARY_PATH"
+    fi
+    LD_LIBRARY_PATH="/usr/local/lib:/usr/lib:/lib:$LD_LIBRARY_PATH"
+    export LD_LIBRARY_PATH
+
+    # Odin
+    export ODIN_ROOT=$HOME/.local/share/distrobox/''${PROFILE}/odin
+
+    exec $SHELL
+  '';
 in
 {
   options.configs.apps.distrobox.enable = lib.mkEnableOption "Distrobox.\nNeeds {option}`nixos.docker.enable`";
@@ -13,6 +35,7 @@ in
     home.packages = with pkgs; [
       distrobox
       boxbuddy
+      distrobox-env
     ];
   };
 }
